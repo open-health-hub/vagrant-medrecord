026898550aa01fc51b21da453a3a4b72b66679ab
Rob Dyke <rob@neovahealth.co.uk>
2014-10-14 23:15:11 +0100

    duplicate instructions in scripts

--- provisioning/load-archetypes.sh
+++ provisioning/load-archetypes.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-cd /opt/medrecord/archetypes
-mvn install
-cd /opt/medrecord/java
-./gradlew vendor
-./gradlew build
-./gradlew integrationTest
-./gradlew tomcatRunWar &
 for i in `ls -1 /opt/medrecord/archetypes/archetypes/openehr/*.adl`; do curl -X POST -T $i -H "Content-Type: text/plain" http://localhost:8100/medrecord/v2/archetype; done
